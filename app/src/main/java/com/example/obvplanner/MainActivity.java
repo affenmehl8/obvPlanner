629d8f1a3db00a713b0cc5292a8705483c468a60
stanscwa <andreas.schwarz_1@stud.hs-kempten.de>
2021-05-12 16:42:37 +0200

    TO: fragments & functioning btns with viewpager2

--- app/src/main/java/com/example/obvplanner/MainActivity.java
+++ app/src/main/java/com/example/obvplanner/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.obvplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    FragmentManager fm = getFragmentManager();
+    QuestsFragment questsFragment = new QuestsFragment();
+    ManageFragment manageFragment = new ManageFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //fm.beginTransaction()
+
+    }
+
+
+
+
+
+    public void leftClick(View view) {
+    }
+
+    public void rightClick(View view) {
+
     }
 }
