9f2aa614bac7db0f6ee6c7112350695fb0f9cc60
laptop <andreas.schwarz99@outlook.com>
2021-05-15 14:43:47 +0200

    todo:mapbox implementieren mit location picker

--- app/src/main/java/com/example/obvplanner/AddQuestActivity.java
+++ app/src/main/java/com/example/obvplanner/AddQuestActivity.java
@@ -1,16 +1,46 @@
 package com.example.obvplanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
 public class AddQuestActivity extends AppCompatActivity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_quest);
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+
+
+                    }
+                });
+
+            }
+        });
     }
 
     public void onClickAddQuest(View view) {
