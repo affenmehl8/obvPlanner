0e15f7909fd4a1ca4505f719018df43c2cf79528
laptop <andreas.schwarz99@outlook.com>
2021-05-17 14:40:12 +0200

    commit AddQuestLocationActivity

---
+++ app/src/main/java/com/example/obvplanner/AddQuestLocationActivity.java
@@ -0,0 +1,75 @@
+package com.example.obvplanner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+public class AddQuestLocationActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private MapboxMap mapboxMap;
+    private ImageView arrow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+        setContentView(R.layout.activity_add_quest_location);
+
+
+
+        arrow = (ImageView) findViewById(R.id.locationPickerArrow);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+
+                AddQuestLocationActivity.this.mapboxMap = mapboxMap;
+
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+
+
+                    }
+                });
+
+            }
+        });
+
+    }
+
+
+
+
+    public void onClickAddQuestLocation(View view) {
+        LatLng position = mapboxMap.getProjection().fromScreenLocation(new PointF(arrow.getLeft()+(arrow.getWidth()/2), arrow.getBottom()));
+
+        Log.d("coords",position.getLatitude()+" "+position.getLongitude()); //latitude wird falsch berechnet!!!
+
+        //daten übergeben ...
+        Intent replyIntent = new Intent();
+
+        replyIntent.putExtra("longitude",position.getLongitude());
+        replyIntent.putExtra("latitude",position.getLatitude());
+        setResult(RESULT_OK,replyIntent);
+        finish();
+    }
+}
